96b0dbcfe05229764cd7f78ba47e6721071bd069
Vivs <129963246+VivieneGarcia@users.noreply.github.com>
2024-11-12 00:56:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -230,8 +230,8 @@ def detect_issues_route():
 
 @app.route('/remove_columns', methods=['POST'])
 def remove_columns():
-    client_ip = request.remote_addr
-    print(f"Request received from IP: {client_ip}")
+    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    print(f"Request received from IPsssss: {client_ip}")
     try:
         data = request.json.get('data')
         columns = request.json.get('columns')
